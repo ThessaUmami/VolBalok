2c6a47ea67f205930749760fc4dc1221a6ea518c
ThessaUmami <37094723+ThessaUmami@users.noreply.github.com>
2018-03-07 19:05:38 +0800

    Add files via upload

---
+++ App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>thessa sahara umami </Text>
+        <Text>Saya Belajar React-Native</Text>
+        <Text>Shake your phone to open the developer menu.</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
